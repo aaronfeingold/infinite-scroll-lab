11c84a0f3c7e5c96651befb5938037ef96806a85
aaron.feingold <ajfeingold88@gmail.com>
2025-05-20 15:49:10 -0500

    new(form.tsx): sa form that will handle some simple descriptions

---
+++ src/components/Form.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Button from "./Button";
+import type { MockItem } from "../utils/fetchMockData";
+
+interface FormProps {
+  onSubmit: (data: Omit<MockItem, "id">) => void;
+  onCancel: () => void;
+}
+
+const Form: React.FC<FormProps> = ({ onSubmit, onCancel }) => {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="space-y-4">
+      <div className="space-y-2 text-white">
+        <label htmlFor="Title" className="block text-sm font-medium text-white">
+          Title
+        </label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+          className="bg-slate-400 text-stone-950 mt-1 w-full border border-green-600 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="description"
+          className="block text-sm font-medium text-white"
+        >
+          Description
+        </label>
+        <textarea
+          id="description"
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+          className="bg-slate-400 mt-1 w-full border border-green-600 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          rows={3}
+          required
+        />
+      </div>
+      <div className="flex justify-end space-x-2">
+        <Button text="Cancel" onClick={onCancel} variant="secondary" />
+        <Button text="Submit" type="submit" variant="primary" />
+      </div>
+    </form>
+  );
+};
+
+export default Form;
