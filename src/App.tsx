bab98d479a31b66989cfc0dae04588f35c5dcc7c
aaron.feingold <ajfeingold88@gmail.com>
2025-05-20 15:46:35 -0500

    update(app.tsx) no we have a handlesubmit function that is passed to the modal, which has a form as children

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,66 @@
+import { useState } from "react";
 import { useInfiniteScroll } from "./hooks/useInfiniteScroll";
+import Modal from "./components/Modal";
+import Form from "./components/Form";
+import Button from "./components/Button";
+import type { MockItem } from "./utils/fetchMockData";
 
 function App() {
-  const { items, loading, hasMore, sentinalRef } = useInfiniteScroll();
+  const { prependItem, items, loading, hasMore, sentinelRef } =
+    useInfiniteScroll();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleSubmit = (data: Omit<MockItem, "id">) => {
+    const newItem: MockItem = {
+      id: items.length + 1,
+      ...data,
+    };
+    prependItem(newItem);
+    setIsModalOpen(false);
+  };
 
   return (
     <div className="bg-gray-900 h-screen min-h-screen flex items-center justify-center p-6">
-      <div className="bg-gray-800 h-full px-6 py-4 flex flex-col rounded shadow max-w-md sm:max-w md:max-w-lg w-full">
+      {/* Main Container */}
+      <div className="bg-gray-800 h-full px-8 py-4 space-y-4 flex flex-col rounded shadow max-w-md sm:max-w md:max-w-lg w-full">
+        {/* Header */}
         <h1 className="text-3xl font-bold text-white mb-6 text-center">
           Infinite Scroll Demo
         </h1>
-
+        {/* Scroll Container */}
         <div
           id="scroll-container"
-          className="bg-gray-500 h-4/5 flex justify-center rounded-lg shadow overflow-y-auto space-y-4"
+          className="bg-gray-500 h-4/5 flex flex-col rounded-lg shadow overflow-y-auto space-y-4 p-4"
         >
-          {/* Content will be added here */}
+          {/* Items List with Infinite Scroll*/}
           {items.map((item) => (
             <div key={item.id} className="p-4 bg-gray-700 rounded-lg shadow">
               <h2 className="text-lg font-bold">{item.title}</h2>
-              <p className="text-gray-700">{item.description}</p>
+              <p className="text-white">{item.description}</p>
             </div>
           ))}
           {loading && (
-            <div className="h-4 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 rounded">
-              <h2 className="text-lg font-bold">Loading...</h2>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center justify-center z-10 bg-gray-900 opacity-50 rounded-lg">
+                <span className="animate-bounce text-white text-lg font-bold">
+                  Loading...
+                </span>
+              </div>
+              <div className="space-y-4">
+                {/* Loading Skeleton */}
+                {Array.from({ length: 3 }).map((_, i) => (
+                  <div
+                    key={i}
+                    className="p-4 bg-gray-700 rounded-lg shadow animate-pulse"
+                  >
+                    <div className="h-4 w-1/3 mb-2 rounded"></div>
+                    <div className="space-y-2">
+                      <div className="h-3 w-full mb-2 rounded"></div>
+                      <div className="h-3 w-5/6 mb-2 rounded"></div>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           )}
           {!hasMore && (
@@ -32,8 +69,20 @@ function App() {
             </div>
           )}
           {/* sentinel div for IntersectionObserver */}
-          <div ref={sentinalRef} />
+          <div ref={sentinelRef} />
         </div>
+        <Button
+          text="Add Item"
+          onClick={() => setIsModalOpen(true)}
+          className="mx-4 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-mint-600"
+        />
+
+        <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+          <Form
+            onSubmit={handleSubmit}
+            onCancel={() => setIsModalOpen(false)}
+          />
+        </Modal>
       </div>
     </div>
   );
